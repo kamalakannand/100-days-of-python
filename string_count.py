7074847a630afba7f6dca390ed01644a598798ed
kamalakannand <dashnakannan@gmail.com>
2024-11-27 15:41:50 +0530

    test

---
+++ string_count.py
@@ -0,0 +1,15 @@
+# import pandas as pd
+# data = {
+#     'Salesperson': ['Alice', 'Alice', 'Alice', 'Bob', 'Bob', 'Charlie', 'Charlie', 'Charlie', 'David', 'David', 'David', 'Eve', 'Eve'],
+#     'Sales': [100, 200, 300, 400, 500, 200, 300, 100, 600, 100, 200, 100, 100]
+# }
+# data['id'] = 100000
+# df = pd.DataFrame(data)
+# sum_df = df.groupby('Salesperson')['Sales'].sum()
+# df['Row_number1'] = df.groupby('Salesperson')['Sales'].cumcount()+1
+# df["Row_number"] = df.reset_index().index+1
+# print(df)
+print("k/nkkkk\nnnnn")
+
+
+
